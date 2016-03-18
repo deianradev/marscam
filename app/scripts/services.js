b689e923eeaee51a650b9bee13fcfe6a33c9dba5
deian <deian.m.radev@gmail.com>
2016-03-17 20:57:00 -0400

    cleaning up code

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -1,36 +1,19 @@
 'use strict';
 
-angular.module('starter.services', [])
+angular.module('starter.services', ['config'])
 
-.service("marsDataService", function($http, $q, $rootScope, $timeout) {
+.service("marsDataService", function($http, $q, $rootScope, $timeout, ENV) {
       var mars = this;
       mars.album = {};
       var page = 0;
 
-      var endPoint1 = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1001&api_key=EONpGrniR1AIRYU0IyMK3UEnDnyprtXPmFT4Sn6q';
-      //var endPoint = '/api/marsapi.json';
-      var endPoint = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos';
-
-      mars.getAllPhotos = function() {
-        var defer = $q.defer();
-
-        $http.get(endPoint1)
-          .success(function(response) {
-            mars.album = response;
-            defer.resolve(response);
-          })
-          .error(function(err, status) {
-            defer.reject(err);
-          })
-        return defer.promise;
-      }
-
       mars.filterPhotos = function(filter) {
+
         var data = {
           sol: filter.sol,
           camera: filter.camera,
           page: filter.page,
-          api_key: 'EONpGrniR1AIRYU0IyMK3UEnDnyprtXPmFT4Sn6q'
+          api_key: ENV.api_key
         }
 
         var config = {
@@ -38,70 +21,14 @@ angular.module('starter.services', [])
           paramsOrder: [data.sol, data.camera, data.page, data.api_key]
         };
 
-        // var params = {
-        //     "sol": 0,
-        //     "page": page++,
-        //     "api_key": 'EONpGrniR1AIRYU0IyMK3UEnDnyprtXPmFT4Sn6q'
-        //   }
-          //$http.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=100&page='+ page +'&api_key=EONpGrniR1AIRYU0IyMK3UEnDnyprtXPmFT4Sn6q')
-        return $http.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?', config)
+        return $http.get(ENV.apiEndpoint, config)
           .then(function(res) {
             console.log(res.data.photos, ' page: ', config.params)
               return res.data
             })
       }
-
-      mars.filterPhotosOld = function(filter) {
-        var defer = $q.defer();
-
-        var data = {
-          sol: filter.sol,
-          camera: filter.camera,
-
-          api_key: 'EONpGrniR1AIRYU0IyMK3UEnDnyprtXPmFT4Sn6q'
-        }
-
-        var config = {
-          params: data,
-          paramsOrder: [data.sol, data.camera, data.api_key]
-        };
-
-        $http.get(endPoint, config)
-          .success(function(response) {
-            mars.album = response;
-            defer.resolve(response);
-          })
-          .error(function(err, status) {
-            defer.reject(err);
-          })
-        return defer.promise;
-      }
-
-      mars.getAllData = function(param) {
-
-        var params = {
-            "sol": 0,
-            "page": page++,
-            "api_key": 'EONpGrniR1AIRYU0IyMK3UEnDnyprtXPmFT4Sn6q'
-          }
-          //$http.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=100&page='+ page +'&api_key=EONpGrniR1AIRYU0IyMK3UEnDnyprtXPmFT4Sn6q')
-        return $http.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?', {
-            params: params
-          })
-          .then(function(res) {
-              return res.data
-            })
-          };
-
+      
         return mars;
-        /*
-          var deferred = $q.defer();
-          $http.get('http://www.json-generator.com/api/json/get/cfvEGqRXQO?indent=2').then(function(data) {
-            deferred.resolve(data);
-          });
-          this.getMarsResults = function() {
-            return deferred.promise;
-          } */
       })
     .factory('Chats', function() {
       // Might use a resource here that returns a JSON array
